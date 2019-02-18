af4965a1a54fdfd373b9e32fce0e289336aecd98
lamyahamid <lamya.altuwairqi@gmail.com>
2019-02-19 01:25:07 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,26 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+
+var cards = ["queen" , "queen" , "king" , "king"];
+var cardsInPlay = [];
+
+var checkForMatch = function ()
+{
+	 if(cardsInPlay[0] == cardsInPlay[1])
+		 alert("You found a match!");
+	 else
+		 alert("Sorry, try again.");
+	
+}
+var flipCard = function(cardId){
+
+
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+ 
+ checkForMatch();
+
+	
+}
+
+flipCard(0);
+flipCard(2);
